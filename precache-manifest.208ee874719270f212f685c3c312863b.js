3563b0121f8daee75e435bc176f7b3f026c7289d
DipenMavani <mavanidipen252@gmail.com>
2020-03-31 16:33:31 +0530

    Updates

---
+++ precache-manifest.208ee874719270f212f685c3c312863b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5e226ccb6fc30f1835f4126ffb8577",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "240fe775e6ea3cf8ed49",
+    "url": "/chat/static/css/main.5a0f12ef.chunk.css"
+  },
+  {
+    "revision": "061c0456c3f129c45fdd",
+    "url": "/chat/static/js/2.45c78d02.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/chat/static/js/2.45c78d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240fe775e6ea3cf8ed49",
+    "url": "/chat/static/js/main.60fef4e0.chunk.js"
+  },
+  {
+    "revision": "eaa968ab2d1506d9167b",
+    "url": "/chat/static/js/runtime-main.d28438c1.js"
+  }
+]);
